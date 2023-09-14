e48bc3380e322539a58652204290246b616d45fa
farnoosh azour <72529423+farnoosh27@users.noreply.github.com>
2023-09-14 11:21:15 -0400

    Add files via upload
    in case you want to change the formatting of your csv file, you can use the delimiter attribute to do so.

---
+++ csv_formatting.py
@@ -0,0 +1,14 @@
+import csv
+
+input_file = 'Llama-2-13b-hf-stuff-quantized-mavenir-DIAMETER.csv'  # Replace with your input file path
+output_file = 'Llama-2-13b-hf-stuff-quantized-converted-mavenir-DIAMETER.csv'  # Replace with your output file path
+
+# Reading the CSV file and writing it with a new delimiter
+with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile, delimiter=';')
+    
+    for row in reader:
+        writer.writerow(row)
+
+print(f"The CSV file has been converted and saved with ';' as the delimiter as '{output_file}'")
